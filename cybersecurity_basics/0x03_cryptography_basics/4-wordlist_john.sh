a6819212920b3a530c96d82960f0498f2971af75
vanessaDH <vanessadham2020@outlook.com>
2026-05-18 19:08:58 -0400

    Add 4-wordlist_john.sh

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --format=raw-sha1 --wordlist=/usr/share/wordlists/rockyou.txt "$1" ; john --show --format=raw-sha1 "$1" | cut -d: -f2 | sed '$d' > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show "$1" | cut -d: -f2 | head -n -1 > 4-password.txt
