48c956cd56e4ce50e5e764faf942939e889bd257
vanessaDH <vanessadham2020@outlook.com>
2026-05-19 07:03:50 -0400

    Add 5-windows_john.sh

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=nt "$1" > 5-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=nt "$1"; john --show --format=nt "$1" | awk -F: 'NF>1 {print $2}' > 5-password.txt
