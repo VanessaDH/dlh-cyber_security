cdd1d5bdcd49d930c0b232e8b9cbb59193a2e22c
vanessaDH <vanessadham2020@outlook.com>
2026-05-25 13:42:10 -0400

    Add 2-harden.sh

--- linux_security/network_protocols/2-harden.sh
+++ linux_security/network_protocols/2-harden.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -type d -perm -o+w 2>/dev/null -exec echo {} \; -exec chmod o-w {} \; 2>/dev/null 
+find / -type d -perm -o+w 2>/dev/null -print -exec echo {} \; -exec chmod o-w {} \; 2>/dev/null 
