c38ea6c9989837c342f0d5b84a83cf47186e8bc6
vanessaDH <vanessadham2020@outlook.com>
2026-05-31 14:57:23 +0000

    Added 6-main.py and 6-netrecon.py

---
+++ scripting_cyber/0x01-python_scripting/6-netrecon.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import socket
+import requests
+import dns.resolver
+from bs4 import BeautifulSoup
+
+def dns_recon(domain):
+	try:
+		ip = socket.gethostbyname(domain)
+		print(f"	IP ADDRESS:	{ip}")
+	except socket.gaierror:
+		print(f"	IP ADDRESS: Failed to resolve")
+
+	try:
+		mx_records = dns.resolver.resolve(domain, dns.rdatatype.MX)
+		for r in mx_records:
+			print(f"	MX Record:	{r.exchange}")
+	except Exception:
+		print(f"	MX Records: None found")
+	
+def web_recon(domain):
+
+	try:
+		response = requests.get(f"http://{domain}", timeout=5)
+		print(f"	Status Code: {response.status_code}")
+		
+		for key in ['Server','Content-TYpe','X-Frame-Options']:
+			if key in response.headers:
+				print(f"	{key}: {response.headers[key]}")
+				
+		soup = BeautifulSoup(response.text, 'html.parser')
+		print(f"	Links Found: {len(soup.find_all('a',href=True))}")
+	except requests.exceptions.RequestException as e:
+		printf(f"	Error: {e}")
+
+def port_scan(domain):
+
+	for port in [80,443]:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.settimeout(1)
+
+			result =  s.connect_ex((domain,port)) == 0
+			s.close()
+
+			print(f"	Port {port}: {'OPEN' if result else 'CLOSED'}")
+		except Exception:
+			print(f"	Port {port}: CLOSED")
