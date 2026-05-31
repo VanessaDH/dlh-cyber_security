11f5b0b546ef94f6ea1414160d0bfe3589666fa5
vanessaDH <vanessadham2020@outlook.com>
2026-05-31 13:16:11 +0000

    Add 1-dns_records.py and 1-main.py

--- scripting_cyber/0x01-python_scripting/1-dns_records.py
+++ scripting_cyber/0x01-python_scripting/1-dns_records.py
@@ -4,13 +4,20 @@ import dns.rdatatype
 
 def query_dns_records(domain_name):
 	results ={}
-	record_types = ['A', 'AAA', 'MX','NS', 'TXT', 'SOA']
+	record_types ={
+		'A':dns.rdatatype.A,
+		'AAAA': dns.rdatatype.AAAA,
+		'MX': dns.rdatatype.MX,
+		'NS': dns.rdatatype.NS, 
+		'TXT': dns.rdatatype.TXT, 
+		'SOA': dns.rdatatype.SOA
+	}
 
 	for record_type in record_types:
 		try:
 			answers = dns.resolver.resolve(domain_name, record_type)
 			results[record_type] = answers
-		except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN, dns.resolver.NoNameservers,dns.rdatatype.UnknownRdatatype):
+		except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN, dns.resolver.NoNameservers):
 			pass
 			
 	return results	
