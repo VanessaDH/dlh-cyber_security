d43b460f9e62e041f975e57cf48c8ceee94ada3d
vanessaDH <vanessadham2020@outlook.com>
2026-05-31 13:05:48 +0000

    Add 1-dns_records.py and 1-main.py

---
+++ scripting_cyber/0x01-python_scripting/1-dns_records.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import dns.resolver
+import dns.rdatatype
+
+def query_dns_records(domain_name):
+	results ={}
+	record_types = ['A', 'AAA', 'MX','NS', 'TXT', 'SOA']
+
+	for record_type in record_types:
+		try:
+			answers = dns.resolver.resolve(domain_name, record_type)
+			results[record_type] = answers
+		except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN, dns.resolver.NoNameservers, dns.exception.DNSException, Exception):
+			pass
+			
+	return results	
